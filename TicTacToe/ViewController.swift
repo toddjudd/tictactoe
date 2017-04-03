21cfd058095a923e8c16f43de9bdf31fd4a6cc0a
Todd Judd <todd.judd@gmail.com>
2017-04-02 19:54:17 -0600

    all working

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -26,7 +26,6 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     var activePlayer = 1
